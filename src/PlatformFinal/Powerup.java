2e783010b270e074902d69fa3f7f9de7c9ebcdaa
civvv <erikjulianbeer@gmail.com>
2019-06-11 09:28:57 -0700

    Powerup created

--- src/PlatformFinal/Powerup.java
+++ src/PlatformFinal/Powerup.java
@@ -1,14 +1,81 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PlatformFinal;
+import java.awt.Color;
+import java.awt.Graphics;
 
-/**
- *
- * @author beere1633
- */
-public class Powerup {
+public class Powerup extends Block implements Collidable {
+
+    private int xSpeed;
+    private int ySpeed;
+
+    public Powerup() {
+        super(200, 200);
+        xSpeed = 3;
+        ySpeed = 1;
+    }
+
+    public Powerup(int x, int y, int wid, int ht, Color col) {
+        super(x, y, wid, ht, col);
+        xSpeed = 3;
+        ySpeed = 1;
+    }
+
+    public void setXSpeed(int xSpd) {
+        xSpeed = xSpd;
+    }
+
+    public void setYSpeed(int ySpd) {
+        ySpeed = ySpd;
+    }
+
+    public void moveAndDraw(Graphics window) {
+        //draws over the old ball with a white ball
+        draw(window, Color.white);
+
+        //draw(window,Color.red); //draws over the old ball with whichever color you want
+        //in case you want to have a trail
+        setxPos(getxPos() + xSpeed);
+        setyPos(getyPos() + ySpeed);
+
+        draw(window);   //draws the Ball
+    }
     
+    public boolean didCollideLeft(Object obj) {
+        Block other = (Block) obj;
+        return getxPos() <= other.getxPos() + other.getWidth() + Math.abs(getXSpeed());
+    }
+
+    public boolean didCollideRight(Object obj) {
+        Block other = (Block) obj;
+        return getxPos() + getWidth() >= other.getxPos() - Math.abs(getXSpeed());
+    }
+
+    public boolean didCollideTop(Object obj) {
+        Block other = (Block) obj;
+        return getyPos() >= other.getyPos() && getyPos() <= other.getyPos() + getHeight();
+    }
+
+    public boolean didCollideBottom(Object obj) {
+        Block other = (Block) obj;
+        return getyPos() + getHeight() >= other.getyPos() && getyPos() + getHeight() <= other.getyPos() + other.getHeight();
+    }
+
+    public boolean equals(Object obj) {
+        Enemy1 other = (Enemy1) obj;
+        if (super.equals(other) && getXSpeed() == other.getXSpeed() && getYSpeed() == other.getYSpeed()) {
+            return true;
+        }
+        return false;
+    }
+
+    public int getXSpeed() {
+        return xSpeed;
+    }
+
+    public int getYSpeed() {
+        return ySpeed;
+    }
+
+    public String toString() {
+        return super.toString() + " " + xSpeed + " " + ySpeed;
+    }
 }
