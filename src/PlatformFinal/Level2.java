c54405fd011e413be751af110737fec29704e625
flybirdcl <cassidy.liu2@gmail.com>
2019-06-12 08:32:13 -0700

    Made persistent data

--- src/PlatformFinal/Level2.java
+++ src/PlatformFinal/Level2.java
@@ -13,14 +13,19 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.*;
 import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 import static java.lang.Character.toUpperCase;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author beere1633
  */
-public class Level2 extends Canvas implements KeyListener, Runnable{
+public class Level2 extends Canvas implements KeyListener, Runnable {
+
     private Player user;
     private ArrayList<Ball> ballList;
     private Enemy1 bob;
@@ -35,13 +40,14 @@ public class Level2 extends Canvas implements KeyListener, Runnable{
     private BufferedImage back;
     private FileWriter writer;
     private ArrayList<Enemy1> enemyList;
+    private int score = 0;
 
     public Level2() {
         Ball ball1 = new Ball(10, 100, 40, 40, Color.ORANGE, 2, 1);
         Ball ball2 = new Ball(20, 100, 40, 40, Color.red, 1, 2);
         Ball ball3 = new Ball(60, 100, 40, 40, Color.green, -4, 2);
         Ball ball4 = new Ball(90, 30, 40, 40, Color.BLACK, 3, 5);
-        Ball ball5 = new Ball(90, 20, 40 , 40, Color.MAGENTA, -5, 2);
+        Ball ball5 = new Ball(90, 20, 40, 40, Color.MAGENTA, -5, 2);
         Ball ball6 = new Ball(50, 130, 40, 40, Color.GRAY, 4, 1);
         ballList = new ArrayList<Ball>();
         ballList.add(ball1);
@@ -50,8 +56,7 @@ public class Level2 extends Canvas implements KeyListener, Runnable{
         ballList.add(ball4);
         ballList.add(ball5);
         ballList.add(ball6);
-        
-        
+
         enemyList = new ArrayList<Enemy1>();
         user = new Player(100, 100, 20, 25, Color.blue, 2);
         Enemy1 e1 = new Enemy1(550, 50, 20, 400, Color.red, 2, 1);
@@ -61,20 +66,20 @@ public class Level2 extends Canvas implements KeyListener, Runnable{
         goal = new Enemy1(600, 260, 30, 30, Color.green);
         keys = new boolean[4];
 
-        
         setBackground(Color.WHITE);
         setVisible(true);
 
         new Thread(this).start();
         addKeyListener(this);
 
-        
     }
 
     public void update(Graphics window) {
         paint(window);
     }
+
     //Paint method from Pong as there isn't any easy way to write a different method for this.
+
     public void paint(Graphics window) {
         Graphics2D twoDGraph = (Graphics2D) window;
         if (back == null) {
@@ -85,47 +90,51 @@ public class Level2 extends Canvas implements KeyListener, Runnable{
         graphToBack.setColor(Color.red);
         goal.draw(graphToBack);
         user.draw(graphToBack);
-        for (Ball b: ballList){
+        for (Ball b : ballList) {
             b.moveAndDraw(graphToBack);
-            for (Enemy1 e: enemyList){
-                if (b.isCollide(e)){
+            for (Enemy1 e : enemyList) {
+                if (b.isCollide(e)) {
                     b.setXSpeed(-b.getXSpeed());
                     b.setYSpeed(-b.getYSpeed());
                 }
-                
+
             }
             if (!(b.getyPos() >= 0 && b.getyPos() <= 450)) {
-            b.setYSpeed(-b.getYSpeed());
+                b.setYSpeed(-b.getYSpeed());
             }
-            if (!(b.getxPos()>=0 && b.getxPos() <= 650)){
+            if (!(b.getxPos() >= 0 && b.getxPos() <= 650)) {
                 b.setXSpeed(-b.getXSpeed());
             }
-            if(b.isCollide(user)){
+            if (b.isCollide(user)) {
                 user.draw(graphToBack, Color.WHITE);
                 user.setxPos(100);
                 user.setyPos(100);
                 user.draw(graphToBack, Color.BLUE);
+                score++;
             }
         }
-        
-        if (goal.isCollide(user)){
+
+        if (goal.isCollide(user)) {
+            try {
+                scorer(score);
+            } catch (IOException ex) {
+                Logger.getLogger(Level2.class.getName()).log(Level.SEVERE, null, ex);
+            }
             System.exit(0);
         }
-        
-        for (Enemy1 e: enemyList){
+
+        for (Enemy1 e : enemyList) {
             e.draw(graphToBack);
-            if (user.isCollide(e)){
+            if (user.isCollide(e)) {
                 user.draw(graphToBack, Color.WHITE);
                 user.setxPos(100);
                 user.setyPos(100);
                 user.draw(graphToBack, Color.BLUE);
+                score++;
             }
         }
 
         //Collision detection: Some problems with up and down, however functional
-
-        
-        
         if (keys[0] == true) {
             user.moveUpAndDraw(graphToBack);
         }
@@ -138,7 +147,7 @@ public class Level2 extends Canvas implements KeyListener, Runnable{
         if (keys[3] == true) {
             user.moveRightAndDraw(graphToBack);
         }
-  
+
         twoDGraph.drawImage(back, null, 0, 0);
     }
 
@@ -178,8 +187,17 @@ public class Level2 extends Canvas implements KeyListener, Runnable{
 
     public void keyTyped(KeyEvent e) {
     }
-    
-    
+
+    public void scorer(int a) throws IOException {
+        a = a - 5;
+        System.out.println("User " + PlayPlatformer.getName1() + " failed " + a + " times in level 2.");
+        BufferedWriter out;
+        out = new BufferedWriter(new FileWriter("Scores.txt", true));
+        out.append("User " + PlayPlatformer.getName1() + " failed " + a + " times in level 2.");
+        out.newLine();
+        out.close();
+        System.exit(0);
+    }
 
     public void run() {
         try {
